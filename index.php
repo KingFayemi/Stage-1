be6d8c78e95c4f86b9c4585a003540fbcad4f91b
Fayemi David <fayemidavid@gmail.com>
2018-04-18 14:04:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<html>
+	<head>
+		<title>HNG Internship 4 Stage1</title>
+
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+		<div id= center>
+		<img src="profile.jpg" class="profile">
+		
+		<h1>UI/UX DESIGN INTERN</h1> <hr>
+		
+		<p>  
+			Hi, I’m Fayemi David, a self taught UI/UX designer in the making from Lagos.<br> 
+			I believe in the “simple is more” principle.<br>
+			I would be joining the HNG internship to improve myself and speed up my learning process.<br>
+			Nice to meet you.
+		</p>
+		<button>
+			<?php
+ 				date_default_timezone_set("Africa/Lagos");
+	    		echo date('h:ia');
+			?>
+		</button>
+
+		</div>
+
+
+	</body>
+
+</html>
